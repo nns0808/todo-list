7e1ffdbc11adcb5be983693f8cc4b5acdc2b11cf
Natalia <nns0808@gmail.com>
2025-07-21 17:47:01 -0700

    completed week-03

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import './App.css'
+import './App.css';
+import { useState } from 'react';
 import TodoList from './TodoList' 
 import TodoForm from './TodoForm';
 
 
 function App() {
-  
+  const [newTodo, setNewTodo] = useState('My first todo');
   return (
   <div>
     <h1>My Todo List</h1>
     <TodoForm />
+    {/* Display the current value of newTodo */}
+    <p>{newTodo}</p>
     <TodoList />
     
   </div>
